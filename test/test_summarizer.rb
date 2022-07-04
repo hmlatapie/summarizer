a864ee93dc621fbcaaff721178305bf2aad16946
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-07-04 14:15:45 -0500

    Initial commit

---
+++ test/test_summarizer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestSummarizer < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Summarizer::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
