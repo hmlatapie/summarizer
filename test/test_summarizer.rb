0954e55f421c6e9f94cd1983772ac1fba578c193
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-07-09 02:39:30 -0500

    Oups! I missed that test

--- test/test_summarizer.rb
+++ test/test_summarizer.rb
@@ -6,8 +6,4 @@ class TestSummarizer < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Summarizer::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
